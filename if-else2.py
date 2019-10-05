a4287a5eb84edfd0d26f2efa9b8381f7ada71ee8
PattanaWattana <patcom.frost@gmail.com>
2019-10-05 09:33:13 +0700

    Create if-else2.py

---
+++ if-else2.py
@@ -0,0 +1,8 @@
+c = 45
+
+if c > 50:
+    print('{} Greater than 50 '.format(c))
+elif c > 40 :
+    print('{} Greater than 40 but less than 50'.format(c))
+else:
+     print('{}Smaller than 40'.format(c))
